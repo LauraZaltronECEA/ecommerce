49e6f558667f582ad8e4974d1a43f777a98ead9c
blauury <lazaltron@fecea.edu.ar>
2026-04-29 16:11:10 -0300

    Atributos de las entidades Cart, Product, Sale, Sale_Information y User

--- api.ecommerce/api.models/Entities/Product.cs
+++ api.ecommerce/api.models/Entities/Product.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace api.models.Entities
 {
-    internal class Product
+    public class Product
     {
+        public int Id { get; set; }
+        public string Product_Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
     }
 }
--- api.ecommerce/api.models/Entities/User.cs
+++ api.ecommerce/api.models/Entities/User.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace api.models.Entities
 {
-    internal class User
+    public class User
     {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
     }
 }
