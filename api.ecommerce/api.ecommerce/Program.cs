9f89c81db1b218527b6b22a746c43ebeed3c0094
blauury <lazaltron@fecea.edu.ar>
2026-05-04 21:50:13 -0300

    feat: api.ecommerce/Program.cs JWT y conexion a sql

--- api.ecommerce/api.ecommerce/Program.cs
+++ api.ecommerce/api.ecommerce/Program.cs
@@ -1,6 +1,83 @@
+using api.services.Handlers;
+using api.services.Repositories;
+using api.services.v1;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Ingresa el token JWT."
+    });
+
+    options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecuritySchemeReference("Bearer", document, null),
+            new List<string>()
+        }
+    });
+});
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(builder =>
+{
+    builder.AllowAnyOrigin();
+    builder.AllowAnyMethod();
+    builder.AllowAnyHeader();
+}));
+
+// JWT
+var jwtSection = builder.Configuration.GetSection("Jwt");
+var secret = jwtSection["Secret"] ?? throw new InvalidOperationException("Jwt: Secret no configurado");
+var issuer = jwtSection["Issuer"] ?? "api.ecommerce";
+var audience = jwtSection["Audience"] ?? "ecommerce";
+
+builder.Services.AddAuthentication("Bearer").AddJwtBearer("Bearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)),
+    };
+});
+
+// SQLite
+SqliteHandler.ConnectionString = builder.Configuration.GetConnectionString("defaultConnection");
+
+// Dependencias
+builder.Services.AddSingleton<IUserRepository, UserService>();
+builder.Services.AddSingleton<IProductRepository, ProductService>();
+builder.Services.AddSingleton<ICartRepository, CartService>();
+builder.Services.AddSingleton<ISaleRepository, SaleService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI(c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "api.ecommerce");
+    c.RoutePrefix = string.Empty;
+});
 
+app.UseCors();
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
